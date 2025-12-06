964d0dccf4ae9a8c48537019424b65050d44d2e9
Devashish1Dey <devashishdey01@gmail.com>
2025-12-06 19:48:36 +0530

    01-folder done

---
+++ app.js
@@ -0,0 +1,10 @@
+export const name = "Harry"
+const arr = [20,30,40,50]
+
+// export are of two types-
+// default export
+// name export
+
+
+
+export default arr
---
+++ script.js
@@ -0,0 +1,10 @@
+// var h1 = document.createElement("h1")
+// h1.innerHTML = "Hello From Js"
+// // console.log(h1)
+// document.body.appendChild(h1)
+
+import a from "./app.js"  // default export
+import { name } from "./app.js";  // named export
+
+console.log(name)
+console.log(a);
