7e53f0a0da3f07db380ee8fe18a15900955988f6
Devashish1Dey <devashishdey01@gmail.com>
2025-12-07 18:33:55 +0530

    Understanding Some Components of react

---
+++ 02-Components/src/Components/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Card() {
+  const age = 20;
+  return (
+    <div>
+      <div className="card">
+        <h1>Devashish Dey</h1>
+        <p>I am {age} years old</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Illum,perspiciatis.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
