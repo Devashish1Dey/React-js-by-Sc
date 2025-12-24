367149e0ffd9a768c6a46713202f315a8c191ef0
Devashish1Dey <devashishdey01@gmail.com>
2025-12-24 09:31:18 +0530

    UI Project Completed By using Components.

---
+++ 07-Ui-Project/src/components/section1/HeroText.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HeroText = () => {
+  return (
+    <div className="p-6">
+      <h3 className="mb-7 text-7xl font-bold">
+        Prospective <br />
+        <span>customer</span> <br /> Segmentation
+      </h3>
+      <p className="text-xl w-[90%] font-medium text-gray-600">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+        enim eligendi ratione neque nihil ecessitatibus voluptate itaque porro
+        iusto veritatis!
+      </p>
+    </div>
+  );
+};
+
+export default HeroText;
