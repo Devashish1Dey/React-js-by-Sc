591e254bfdb506edabea717f8f74674c776056bd
Devashish1Dey <devashishdey01@gmail.com>
2026-02-23 10:01:42 +0530

    Understanding routing Advanced

---
+++ 19-routing-advanced/src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+// import { useNavigate } from "react-router-dom";
+const About = () => {
+  // const navigate = useNavigate();
+
+  // const btnClicked = () =>{
+  //   // console.log('button is clicked');
+  //   navigate('/')
+  // }
+
+  return (
+    <div>
+      
+      <h1>About Page</h1>
+    </div>
+  );
+};
+
+export default About;
